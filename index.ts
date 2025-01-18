e9bb551a97e4b5188fceef2eea3081ae9e558d0d
Felipe Rohde <feliperohdee@gmail.com>
2025-01-18 11:29:46 -0300

    chore: update package version to 1.0.17 and add promise utility functions to index.ts export

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { promiseFilter, promiseMap, promiseReduce } from './promise-operators.js';
 import promiseAll from './promise-all.js';
 import promiseAllSettled from './promise-all-settled.js';
 import PromiseQueue from './promise-queue.js';
@@ -5,4 +6,4 @@ import retryFn from './retry-fn.js';
 import type { RetryFn, RetryFnContext, RetryFnOptions } from './retry-fn.js';
 
 export type { RetryFn, RetryFnContext, RetryFnOptions };
-export { promiseAll, promiseAllSettled, PromiseQueue, retryFn };
+export { promiseAll, promiseAllSettled, promiseFilter, promiseMap, promiseReduce, PromiseQueue, retryFn };
